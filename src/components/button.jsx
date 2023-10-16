827b03d1fd95e1ed159e299bf8465706d5ad7970
dimdimah <dimahluthfi15@gmail.com>
2023-10-16 23:25:42 +0700

    feat: remove unnecessary file & fixing typo

---
+++ src/components/button.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+export default function Button(props) {
+  const { label } = props;
+  return (
+    <button className="btn btn-neutral btn-wide" {...props}>
+      {label}
+    </button>
+  );
+}
