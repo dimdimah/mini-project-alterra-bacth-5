3d67e7e5486c37dc41d28009960dcbed06236637
dimdimah <dimahluthfi15@gmail.com>
2023-10-25 14:32:19 +0700

    feat: slicing ui products page

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -1,5 +1,34 @@
 import Layout from "@/components/layout";
+import Card from "@/components/modal";
 
-export default function Index() {
-  return <Layout>Product page</Layout>;
+function App() {
+  return (
+    <Layout>
+      <button
+        className="bg-[#565564] rounded-lg"
+        onClick={() => document.getElementById("my_modal_4").showModal()}
+      >
+        <img
+          src="https://react-tailwind-portfolio-virid.vercel.app/Frame2.png"
+          alt="img-products"
+          width={250}
+          height={300}
+        />
+      </button>
+      <dialog id="my_modal_4" className="modal">
+        <div className="w-11/12 max-w-5xl modal-box bg-slate-600">
+          <h3 className="text-lg font-bold">Hello!</h3>
+          <p className="py-4">Click the button below to close</p>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    </Layout>
+  );
 }
+
+export default App;
